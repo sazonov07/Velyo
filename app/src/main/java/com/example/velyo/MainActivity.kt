4002a28590182d44ab71f30b02d60abe38d220e3
Андрей Сазонов <Sazonov07@mail.ru>
2026-01-28 19:14:31 +0300

    add buttons + db

--- app/src/main/java/com/example/velyo/MainActivity.kt
+++ app/src/main/java/com/example/velyo/MainActivity.kt
@@ -3,45 +3,28 @@ package com.example.velyo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.room.Room
+import com.example.velyo.data.db.AppDatabase
+import com.example.velyo.ui.WaterScreen
 import com.example.velyo.ui.theme.VelyoTheme
+import com.example.velyo.vm.WaterViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+        val db = Room.databaseBuilder(
+            applicationContext,
+            AppDatabase::class.java,
+            "velyo.db"
+        ).build()
+
+        val vm = WaterViewModel(db.waterDao())
+
         setContent {
             VelyoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                WaterScreen(vm)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    VelyoTheme {
-        Greeting("Android")
     }
 }
